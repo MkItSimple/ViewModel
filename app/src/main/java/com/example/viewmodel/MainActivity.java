836fdbaba98ace183c30c69d4387af67fae874de
MkItSimple <choanania1@gmail.com>
2020-02-27 21:38:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/viewmodel/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.viewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.DialogTitle;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Button addScoreButton, resetScoreButton;
+    TextView scoreTextView;
+
+    ScoreViewModel scoreViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scoreTextView = findViewById(R.id.scoreTextView);
+        addScoreButton = findViewById(R.id.addScoreButton);
+        resetScoreButton = findViewById(R.id.resetScoreButton);
+
+        scoreViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+
+        scoreViewModel.getScore().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                Log.i(TAG, "onChanged: ");
+                scoreTextView.setText(String.valueOf(integer));
+            }
+        });
+
+        addScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addScore();
+            }
+        });
+
+        resetScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetScore();
+            }
+        });
+    }
+
+    public void addScore() {
+        scoreViewModel.addScore();
+    }
+
+    public void resetScore() {
+        scoreViewModel.resetScore();
+    }
+}
---
+++ app/src/main/java/com/example/viewmodel/ScoreViewModel.java
@@ -0,0 +1,27 @@
+package com.example.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+
+    private MutableLiveData<Integer> score;
+
+    public MutableLiveData<Integer> getScore() {
+        if (score == null) {
+            score = new MutableLiveData<>();
+            resetScore();
+        }
+        return score;
+    }
+
+    public void addScore() {
+        if (score.getValue() != null) {
+            score.setValue(score.getValue() + 1);
+        }
+    }
+
+    void resetScore() {
+        score.setValue(0);
+    }
+}
